632bff60a2eae0877a39c90a382a0220a4607ef0
Oscar Illerup <oillerup@gmail.com>
2024-01-17 11:01:35 +0100

    Comments in functions

--- mqtttestazure/function_app.py
+++ mqtttestazure/function_app.py
@@ -5,9 +5,10 @@ import datetime
 
 
 # MQTT settings
-MQTT_BROKER = "mqtt.eclipseprojects.io"
-MQTT_PORT = 1883
-MQTT_TOPIC_temp = "/topic/oscarillerup/rawtempdata"
+MQTT_BROKER = "mqtt.eclipseprojects.io"                         #the mqtt server url
+MQTT_PORT = 1883                                                #the mqtt server port
+#The topics to subscribe on
+MQTT_TOPIC_temp = "/topic/oscarillerup/rawtempdata"             
 MQTT_TOPIC_hum = "/topic/oscarillerup/rawhumdata"
 MQTT_TOPIC_light = "/topic/oscarillerup/rawlightdata"
 MQTT_TOPIC_soil = "/topic/oscarillerup/rawsoildata"
@@ -16,11 +17,13 @@ MQTT_TOPIC_plight = "/topic/oscarillerup/lightprocesseddata"
 
 
 # MySQL settings
-DB_HOST = "cp07.nordicway.dk"
-DB_USER = "basicwea_eMiot-oillerup121202"
-DB_PASSWORD = "OscarDtu123"
-DB_NAME = "basicwea_test"
-TABLE_NAME_temp = "temp"
+DB_HOST = "cp07.nordicway.dk"                                   #Mysql db host address
+DB_USER = "basicwea_eMiot-oillerup121202"                       #The username that have access to the db you want to connect to
+DB_PASSWORD = "OscarDtu123"                                     #The password for the username that have access to the db you want to connect to
+DB_NAME = "basicwea_test"                                       #The name of the db that you want to connect to
+
+#db tabel names
+TABLE_NAME_temp = "temp"                                        
 TABLE_NAME_hum = "hum"
 TABLE_NAME_light = "light"
 TABLE_NAME_soil = "soil"
@@ -30,9 +33,9 @@ TABLE_NAME_plight = "plight"
 # MQTT callback function
 def on_message(client, userdata, msg):
     try:
+        #decodes the payload from the incomming msg
         payload = json.loads(msg.payload.decode("utf-8"))
-        
-        
+         
         # Extract the data you want to store
         data_to_store = payload
 
@@ -40,6 +43,7 @@ def on_message(client, userdata, msg):
         current_time = datetime.datetime.now()
         formatted_time = current_time.strftime("%Y-%m-%d-%H:%M:%S")
 
+        #the following if statment determent were to store the data, by looking at the msg.topic
         if msg.topic == "/topic/oscarillerup/rawtempdata":
             connection = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
             cursor = connection.cursor()
@@ -108,11 +112,15 @@ def on_message(client, userdata, msg):
         print(f"Error: {e}")
 
 # MQTT setup
-client = mqtt.Client()
-client.on_message = on_message
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-client.subscribe(MQTT_TOPIC_temp)
+client = mqtt.Client()                      #Makes a mqtt client 
+client.on_message = on_message              #Defines what to do when an incoming msg is received 
+client.connect(MQTT_BROKER, MQTT_PORT, 60)  #Connects to the mqtt broker
+
+#Subscribe to the topices that is of interest 
+client.subscribe(MQTT_TOPIC_temp)           
 client.subscribe(MQTT_TOPIC_hum)
 client.subscribe(MQTT_TOPIC_soil)
 client.subscribe(MQTT_TOPIC_light)
+
+#lisens forever on the subscribed channels
 client.loop_forever()
